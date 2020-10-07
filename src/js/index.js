ff5f2c2a8d97898dbd2b2dcb4777a6f2cbca24b0
Ariane Zanardi Francisco <arianezf@gmail.com>
2020-10-07 11:39:26 +0100

    código dado

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 const dado = document.getElementById('dado');
 const button = document.getElementById('button');
 const resultado = document.getElementById('resultado');
+const historial = document.getElementById('historial');
 
 function lanzarDado() {
 
     let numero = Math.random() * (7 - 1) + 1;
     numero = Math.floor(numero);
     resultado.textContent = numero;
+    addHistorial(numero);
+}
+
+
+function addHistorial(numero){
+    console.log(numero);
+
+    let his = document.createElement("div");
+    his.innerHTML = "Resultado:" + numero;
+    document.getElementById('historial').appendChild(his);
 }
 
 button.addEventListener('click', lanzarDado);
